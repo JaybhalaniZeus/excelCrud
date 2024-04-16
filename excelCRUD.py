8459fe63f544d69a1e61ff56d5ee43d3a9702a2e
JaybhalaniZeus <155626954+JaybhalaniZeus@users.noreply.github.com>
2024-04-16 13:25:38 +0530

    Add files via upload

---
+++ excelCRUD.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def read_excel_file(file_path):
+    try:
+        # Read the Excel file into a DataFrame
+        df = pd.read_excel(file_path)
+        
+        # Display the DataFrame
+        print("Contents of Excel file:")
+        print(df)
+        
+        # Return the DataFrame if needed for further processing
+        return df
+    
+    except Exception as e:
+        print("Error:", e)
+
+# Specify the path to the Excel file
+excel_file_path = r"C:\Users\jaybh\OneDrive\Desktop\task.xlsx"
+
+# Call the function to read the Excel file
+read_excel_file(excel_file_path)
